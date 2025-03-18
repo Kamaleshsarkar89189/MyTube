b7c248ec85b9d475eb5b07615c3617585868ba79
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2025-03-19 02:49:43 +0530

    part 1 completed

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -2,23 +2,41 @@
 
 import { Button } from "@/components/ui/button"
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { UserCircleIcon } from "lucide-react"
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react"
+import Link from "next/link";
 
 export const AuthButton = () => {
     return (
         <>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
+            <SignedIn>
+                {/* If you want to customize more.. */}
+                <Button asChild variant="secondary">
+                <Link href="/studio">
+                <ClapperboardIcon />
+                Studio
+                </Link>
+            </Button>
+                <UserButton>
+                    <UserButton.MenuItems>
+                        {/* Add menu item for Studio and User profile */}
+                        <UserButton.Link
+                            label="Studio"
+                            href="/studio"
+                            labelIcon={<ClapperboardIcon className="size-4" />}
+                        />
+                        <UserButton.Action label="manageAccount" />
+                    </UserButton.MenuItems>
+                </UserButton>
+            </SignedIn>
             <SignedOut>
                 <SignInButton mode="modal">
-                <Button
-                    variant="outline"
-                    className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
-                >
-                    <UserCircleIcon />
-                    Sign in
-                </Button>
+                    <Button
+                        variant="outline"
+                        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
+                    >
+                        <UserCircleIcon />
+                        Sign in
+                    </Button>
                 </SignInButton>
             </SignedOut>
         </>
