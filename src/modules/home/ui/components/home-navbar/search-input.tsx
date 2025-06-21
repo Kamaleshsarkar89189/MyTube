5ac6bfd91c6a78252e6a3c0d202f4cedbdaff7d6
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2025-06-21 22:17:13 +0530

    SEO modified

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -23,54 +23,55 @@ export const SearchInputSuspense = () => {
 
     const [value, setValue] = useState(query);
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) =>{
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const url = new URL("/search", APP_URL);
-        const newQuery = value.trim();
+        const cleanedQuery = value.trim().replace(/\s+/g, " ");
 
-        url.searchParams.set("query", encodeURIComponent(newQuery));
+        const url = new URL("/search", APP_URL);
+
+        if (cleanedQuery) {
+            url.searchParams.set("query", cleanedQuery);
+        } else {
+            url.searchParams.delete("query");
+        }
 
         if (categoryId) {
             url.searchParams.set("categoryId", categoryId);
         }
-        
-        if (newQuery === "") {
-            url.searchParams.delete("query");
-        } 
 
-        setValue(newQuery);
+        setValue(cleanedQuery);
         router.push(url.toString());
     }
-        return (
-            <form className="flex w-full max-w-[600px]" onSubmit={handleSearch}>
-                <div className="relative w-full">
-                    <input
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        type="text"
-                        placeholder="Search"
-                        className="w-full pl-4 py-2 pr-12 bg-muted rounded-l-full border border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500" 
-                        />
-                        {value && (
-                            <Button
-                             type="button"
-                             variant="ghost"
-                             size="icon"
-                             onClick={() => setValue("")}
-                             className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full"
-                            >
-                                <XIcon className="text-gray-500"/>
-                            </Button>
-                        )}
-                </div>
-                <button
-                    disabled={!value.trim()}
-                    type="submit"
-                    className="px-5 py-2 bg-muted-foreground bg-gray-100 border border-gray-300 border-l-0 rounded-r-full hover:bg-gray-90 disabled:opacity-50 disabled:cursor-not-allowed"
-                >
-                    <SearchIcon className="size-5" />
-                </button>
-            </form>
-        )
-    }
+    return (
+        <form className="flex w-full max-w-[600px]" onSubmit={handleSearch}>
+            <div className="relative w-full">
+                <input
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    type="text"
+                    placeholder="Search"
+                    className="w-full pl-4 py-2 pr-12 bg-muted rounded-l-full border border-gray-300 dark:border-gray-600 focus:outline-none focus:border-blue-500"
+                />
+                {value && (
+                    <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        onClick={() => setValue("")}
+                        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full"
+                    >
+                        <XIcon className="text-gray-500" />
+                    </Button>
+                )}
+            </div>
+            <button
+                disabled={!value.trim()}
+                type="submit"
+                className="px-5 py-2 bg-muted-foreground bg-gray-100 border border-gray-300 border-l-0 rounded-r-full hover:bg-gray-90 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+                <SearchIcon className="size-5" />
+            </button>
+        </form>
+    )
+}
