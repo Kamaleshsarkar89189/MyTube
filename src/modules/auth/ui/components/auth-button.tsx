499b82ca2754d7d4ea391d6a7af32a57a4d57e41
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2025-07-03 23:22:08 +0530

    added logo and user signup text removed

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -40,7 +40,7 @@ export const AuthButton = () => {
                         className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none"
                     >
                         <UserCircleIcon />
-                        Sign in
+                        {/* Sign in */}
                     </Button>
                 </SignInButton>
             </SignedOut>
--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -45,8 +45,8 @@ export const HomeNavbar = () => {
                     <SidebarTrigger />
                     <Link prefetch href="/" className="hidden md:block">
                         <div className="p-4 flex items-center gap-1">
-                            <Image src="/logo.svg" alt="Logo" width={32} height={32} />
-                            <p className="text-xl font-semibold tracking-tight text-foreground">MyTube</p>
+                            <Image src="/mvhub.png" alt="Logo" width={72} height={32} />
+                            <p className="text-xl font-semibold tracking-tight text-foreground">MovieHub</p>
                         </div>
                     </Link>
                 </div>
