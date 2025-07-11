7269da9c5c5f2bd2596bd7f09593f68dc237fbef
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2025-07-11 21:08:25 +0530

    Url issue

--- src/modules/movie/ui/components/category-button.tsx
+++ src/modules/movie/ui/components/category-button.tsx
@@ -9,17 +9,17 @@ const categories: Category[] = [
     {
         label: "Bollywood Movies",
         colorClass: "bg-green-600",
-        link: "mv-hub.netlify.app/?categoryId=c4459e46-d2c9-4c6c-a878-922b3db79662", // TODO: Add specific link
+        link: "http://localhost:3000/?categoryId=c4459e46-d2c9-4c6c-a878-922b3db79662", // TODO: Add specific link
     },
     {
         label: "Hindi Dubbed Anime",
         colorClass: "bg-red-600",
-        link: "mv-hub.netlify.app/?categoryId=8df947ff-1bd5-4f78-b075-d7a48ac98b25",// TODO: Add specific link
+        link: "http://localhost:3000/?categoryId=8df947ff-1bd5-4f78-b075-d7a48ac98b25",// TODO: Add specific link
     },
     {
         label: "Hollywood Movies",
         colorClass: "bg-yellow-600",
-        link: "mv-hub.netlify.app/?categoryId=6ef6af5b-ba7c-42d2-a7fa-ae5daa8a755e",
+        link: "http://localhost:3000/?categoryId=6ef6af5b-ba7c-42d2-a7fa-ae5daa8a755e",
     },
     {
         label: "Join Our Telegram",
