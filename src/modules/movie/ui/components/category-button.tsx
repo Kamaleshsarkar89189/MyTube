be093d249658152195bb2959d9473cb75b3df98c
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2025-07-11 21:12:18 +0530

    Revisit

--- src/modules/movie/ui/components/category-button.tsx
+++ src/modules/movie/ui/components/category-button.tsx
@@ -9,29 +9,29 @@ const categories: Category[] = [
     {
         label: "Bollywood Movies",
         colorClass: "bg-green-600",
-        link: "http://localhost:3000/?categoryId=c4459e46-d2c9-4c6c-a878-922b3db79662", // TODO: Add specific link
+        link: "https://mv-hub.netlify.app/?categoryId=c4459e46-d2c9-4c6c-a878-922b3db79662",
     },
     {
         label: "Hindi Dubbed Anime",
         colorClass: "bg-red-600",
-        link: "http://localhost:3000/?categoryId=8df947ff-1bd5-4f78-b075-d7a48ac98b25",// TODO: Add specific link
+        link: "https://mv-hub.netlify.app/?categoryId=8df947ff-1bd5-4f78-b075-d7a48ac98b25",
     },
     {
         label: "Hollywood Movies",
         colorClass: "bg-yellow-600",
-        link: "http://localhost:3000/?categoryId=6ef6af5b-ba7c-42d2-a7fa-ae5daa8a755e",
+        link: "https://mv-hub.netlify.app/?categoryId=6ef6af5b-ba7c-42d2-a7fa-ae5daa8a755e",
     },
     {
         label: "Join Our Telegram",
         colorClass: "bg-sky-500",
         link: "https://t.me/AnimeEmperorhindi",
-      },
+    },
 ];
 
 export const CategoryButtons = () => {
     const handleRedirect = (url: string) => {
         window.location.href = url;
-      };
+    };
     return (
         <div className="bg-muted text-white">
             <div className="flex flex-wrap gap-2 justify-center mb-6">
@@ -48,4 +48,3 @@ export const CategoryButtons = () => {
         </div>
     );
 };
-  
